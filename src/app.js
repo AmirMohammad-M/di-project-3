6610daa8068e20c35ccc7709167ed826afb682c0
AmirMohammad Mohseni <amirm97@live.com>
2019-06-12 23:42:45 +0430

    home page with interestings.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import QuestionRoutes from './routes/QuestionRoutes'
 import AnswerRoutes from './routes/AnswerRoutes'
 import CommentRoutes from './routes/CommentRoutes'
 import VoteRoutes from './routes/VoteRoutes'
+import { QuestionTopic, Topic, Question } from './models'
 
 const app = express()
 
@@ -26,6 +27,20 @@ app.use('/answer', AnswerRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/home', isAuthorized, async (req, res) => {
+  const ft = req.user.favoriteTopics[0]
+  const topic = await Topic.findOne({ code: ft })
+  const questions = await QuestionTopic({ topic: topic._id }).lean()
+  const interestings = await Promise.map(questions, async (q) => {
+    const qu = await Question.findById(q).lean()
+    return { question: qu.question, upvotesCount: qu.upvotesCount, id: qu._id }
+  })
+  interestings = interestings.sort((a, b) => {
+    if (a.upvotesCount > b.upvotesCount) return -1
+    return 1
+  })
+  res.json({ interestings })
 })
 
 upsertSU().then(console.log)
