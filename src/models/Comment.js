e1ca07efc848e6942f56b4b6b42a98b2bcd401ce
AmirMohammad Mohseni <amirm97@live.com>
2019-06-16 15:47:38 +0430

    bug.

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -5,7 +5,7 @@ const schema = new Schema({
   commenter: { type: Schema.Types.ObjectId, required: true },
   commentOn: { type: String, required: true, enum: ['Q', 'A'] },
   replyTo: { type: Schema.Types.ObjectId, required: true, index: true },
-  replyToComment: { type: Schema.Types.ObjectId },
+  replyToComment: { type: Schema.Types.ObjectId, default: null },
   upvotesCount: { type: Number, default: 0 },
   commentedAt: { type: Date, default: Date.now }
 })
